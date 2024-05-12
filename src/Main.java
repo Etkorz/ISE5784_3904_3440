3ccde966477ca5aca60c5a50c2406648f3420e91
Eti Koritz <ekoritz@g.jct.ac.il>
2024-05-12 21:12:07 +0300

    geometries and primitives

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+/**
+ * Eti Kenig 213873904
+ * eti6643011@gmail.com
+ * Gitty Shapira  323773440
+ * gittys8273@gmail.com
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
