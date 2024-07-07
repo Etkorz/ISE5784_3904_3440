ea5370051e380cded83ab63c7a53e346c04a4fd6
GittyShapira <gshapira@g.jct.ac.il>
2024-07-07 15:36:16 +0300

    stage 6

--- src/lighting/DirectionalLight.java
+++ src/lighting/DirectionalLight.java
@@ -37,6 +37,6 @@ public class DirectionalLight extends Light implements LightSource{
      */
     @Override
     public Vector getL(Point p){
-        return direction;
+        return direction.normalize();
     }
 }
--- src/renderer/SimpleRayTracer.java
+++ src/renderer/SimpleRayTracer.java
@@ -1,52 +1,101 @@
 package renderer;
 
-import primitives.Color;
-import primitives.Point;
-import primitives.Ray;
+import static geometries.Intersectable.GeoPoint;
+import static primitives.Util.alignZero;
+
+import lighting.LightSource;
+import primitives.*;
 import scene.Scene;
-import geometries.Intersectable.GeoPoint;
 
 import java.util.List;
 
 /**
- * The SimpleRayTracer class represents a basic implementation of the abstract class RayTracerBase.
- * It provides a method to trace a ray in the scene.
- *
- * @author Eti Kenig and Gitty Shapira
+ * This class is an extended class of a ray tracer.
  */
 public class SimpleRayTracer extends RayTracerBase {
     /**
-     * Constructs a RayTracerBasic object with the given scene.
-     *
-     * @param scene the scene to trace rays in
+     * Construct
+     * @param scene
      */
     public SimpleRayTracer(Scene scene) {
-
         super(scene);
     }
 
     /**
-     * Trace the ray and returns the color of the closest body that the ray hits
-     *
-     * @param ray to trace
-     * @return color
+     * @param ray
+     * @return the color of the closest point
      */
     @Override
     public Color traceRay(Ray ray) {
-        List<GeoPoint> points = scene.geometries.findGeoIntersections(ray);
-        return points == null ? scene.background : calcColor(ray.findClosestGeoPoint(points));
-
+        List <GeoPoint> intersections = scene.geometries.findGeoIntersections(ray);
+        if(intersections == null)
+            return scene.background;
+        return calcColor(ray.findClosestGeoPoint(intersections),ray);
     }
 
     /**
-     * return the color of the lighting
      *
-     * @param gp color
-     * @return color of point
+     * @param point - GoePoint
+     * @return the ambient light color
      */
-    private Color calcColor(GeoPoint gp) {
-        return scene.ambientLight.getIntensity()
-                .add(gp.geometry.getEmission());
+    private Color calcColor(GeoPoint point, Ray ray) {
+        return calcLocalEffects(point, ray).add(super.scene.ambientLight.getIntensity());
+    }
+    private Color calcLocalEffects(GeoPoint geoPoint, Ray ray) {
+        Color color = geoPoint.geometry.getEmission();
+        Vector v = ray.getDirection();
+        Vector n = geoPoint.geometry.getNormal(geoPoint.point);
+        double nv = alignZero(n.dotProduct(v));
+        if (nv == 0)
+            return color;
+        Material material = geoPoint.geometry.getMaterial();
+        for (LightSource lightSource : scene.lights) {
+            Vector l = lightSource.getL(geoPoint.point);
+            double nl = alignZero(n.dotProduct(l));
+            if (nl * nv > 0) {
+                Color iL = lightSource.getIntensity(geoPoint.point);
+                Color calcDiffusive = calcDiffusive(material, nl,iL);
+                Color calcSpecular = calcSpecular(material, n, l, nl, v, iL);
+                color = color.add(calcDiffusive, calcSpecular);
+            }
+
+        }
+        return color;
+    }
+
+    /**
+     * Calculation of specular light component
+     *
+     * @param material Attenuation coefficient for specular light component
+     * @param n        normal to point
+     * @param l        direction vector from light to point
+     * @param v        direction of ray shot to point
+     * @param intensity
+     * @return Color of specular light component
+     */
+    private Color calcSpecular(Material material, Vector n, Vector l, double nl, Vector v, Color intensity) {
+        Vector r = l.add(n.scale(-2 * nl));
+        double minusVR = -alignZero(r.dotProduct(v));
+        if (minusVR <= 0)
+            return Color.BLACK; // view from direction opposite to r vector
+//        double pow;
+//        pow = Math.pow(minusVR);
+        Double3 amount;
+        amount = material.kS.scale(Math.pow(minusVR,2));
+        return intensity.scale(amount);
+    }
+
+    /**
+     * Calculation of diffusion light component
+     *
+     * @param material normal to point
+     * @param nl       dot product between n-normal to point and l-direction vector from light to point
+     * @param intensity
+     * @return Color of diffusion light component
+     */
+    private Color calcDiffusive(Material material, double nl, Color intensity) {
+        double abs = Math.abs(nl);
+        Double3 scale = material.kD.scale(abs);
+        return intensity.scale(scale);
     }
 }
-
